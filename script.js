bfb4db524184077accad7b6cca0df09594fbfc0b
dreyhiflden <dreyhiflden@gmail.com>
2019-01-18 14:36:51 +0200

    Change 3 task

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Task 1
-/([0-1][0-9]|[2][0-3]):[0-5][0-9]/g
+// /([0-1][0-9]|[2][0-3]):[0-5][0-9]/g
 
 // Task 2
-/(-?\d+|-?\d+\.\d+)\s?(\*|\/|-|\+)\s?(-?\d+\.?\d+|-?\d+)/g;
+// /(-?\d+|-?\d+\.\d+)\s?(\*|\/|-|\+)\s?(-?\d+\.?\d+|-?\d+)/g;
 
 // Task 3
 function createURL(templateURL, params) {
-  let regEx = /({\bcountry\b})|({\bregion\b})/g;
+  let regEx = /{\w+}/g;
   return templateURL.replace(regEx, addParams);
 
   function addParams(match) {
@@ -16,10 +16,10 @@ function createURL(templateURL, params) {
 }
 
 let url = createURL(
-  `/api/countries/{country}/regions/{region}/`,
-  { country: `Ukraine`, region: `Kiev` }
+  `/api/countries/{country}/regions/{region}/user/{userId}/`,
+  { country: `Ukraine`, region: `Kiev`, userId: `100` }
 );
 
 console.assert(
-  url === '/api/countries/Ukraine/regions/Kiev/'
+  url === '/api/countries/Ukraine/regions/Kiev/user/100/'
 );
